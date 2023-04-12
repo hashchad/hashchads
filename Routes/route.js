fc42b430ab38627e6edf37fdfca8f3d08c4b7274
north <thehashgeek@gmail.com>
2023-04-12 11:03:11 +0100

    consumed one api

--- Routes/route.js
+++ Routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const getData = require('../requests/getData');
 const route = express.Router();
 
 
@@ -8,8 +9,10 @@ const route = express.Router();
 // route.get('/', (req, res, next) => {
 //     res.render('index', { title: 'Dashboard', page_title: 'Dashboard', folder: 'Dashboards' });
 // })
-route.get('/', (req, res, next) => {
-    res.render('dashboard-crypto', { title: 'Dashboard', page_title: 'Dashboard', folder: 'Dashboards' });
+route.get('/', async (req, res, next) => {
+    var nftData = await getData("https://api.saucerswap.finance/tokens");
+    console.log(nftData)
+    res.render('dashboard-crypto', { title: 'Dashboard', page_title: 'Dashboard', folder: 'Dashboards', nftData: nftData });
 })
 route.get('/index', (req, res, next) => {
     res.render('index', { title: 'Dashboard', page_title: 'Dashboard', folder: 'Dashboards' });
@@ -28,6 +31,7 @@ route.get('/apps-crypto-buy-sell', (req, res, next) => {
     res.render('apps-crypto-buy-sell', { title: 'Buy & Sell', page_title: 'Buy & Sell', folder: 'Crypto' });
 })
 
+// https://api.saucerswap.finance/tokens
 
 route.get('/apps-crypto-orders', (req, res, next) => {
     res.render('apps-crypto-orders', { title: 'Orders', page_title: 'Orders', folder: 'Crypto' });
@@ -41,5 +45,42 @@ route.get('/apps-crypto-ico', (req, res, next) => {
 route.get('/apps-crypto-kyc', (req, res, next) => {
     res.render('apps-crypto-kyc', { title: 'KYC Application', page_title: 'KYC Application', folder: 'Crypto' });
 })
+
+// NFT Market Place
+route.get('/apps-nft-marketplace', (req, res, next) => {
+    res.render('apps-nft-marketplace', { title: 'Marketplace', page_title: 'Marketplace', folder: 'NFT Marketplace' });
+})
+route.get('/apps-nft-explore', (req, res, next) => {
+    res.render('apps-nft-explore', { title: 'Explore Now', page_title: 'Explore Now', folder: 'NFT Marketplace' });
+})
+route.get('/apps-nft-auction', (req, res, next) => {
+    res.render('apps-nft-auction', { title: 'Live Auction', page_title: 'Live Auction', folder: 'NFT Marketplace' });
+})
+route.get('/apps-nft-item-details', (req, res, next) => {
+    res.render('apps-nft-item-details', { title: 'Item Details', page_title: 'Item Details', folder: 'NFT Marketplace' });
+})
+route.get('/apps-nft-collections', (req, res, next) => {
+    res.render('apps-nft-collections', { title: 'Collections', page_title: 'Collections', folder: 'NFT Marketplace' });
+})
+route.get('/apps-nft-creators', (req, res, next) => {
+    res.render('apps-nft-creators', { title: 'Creators', page_title: 'Creators', folder: 'NFT Marketplace' });
+})
+route.get('/apps-nft-ranking', (req, res, next) => {
+    res.render('apps-nft-ranking', { title: 'Ranking', page_title: 'Ranking', folder: 'NFT Marketplace' });
+})
+route.get('/apps-nft-wallet', (req, res, next) => {
+    res.render('apps-nft-wallet', { title: 'Wallet Connect', page_title: 'Wallet Connect', folder: 'NFT Marketplace' });
+})
+route.get('/apps-nft-create', (req, res, next) => {
+    res.render('apps-nft-create', { title: 'Create NFT', page_title: 'Create NFT', folder: 'NFT Marketplace' });
+})
+
+route.get('/apps-file-manager', (req, res, next) => {
+    res.render('apps-file-manager', { layout: 'layout/layout-without-bradcrumb', title: 'Create NFT', page_title: 'Create NFT', folder: 'NFT Marketplace' });
+})
+route.get('/apps-todo', (req, res, next) => {
+    res.render('apps-todo', { layout: 'layout/layout-without-bradcrumb', title: 'Create NFT', page_title: 'Create NFT', folder: 'NFT Marketplace' });
+})
+
 
 module.exports = route;

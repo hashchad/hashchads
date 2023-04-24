0446530bc65ee9c7b2316275c56f450c8a74571c
north <thehashgeek@gmail.com>
2023-04-24 02:27:16 +0100

    switched to react

--- server/index.js
+++ server/index.js
@@ -16,8 +16,10 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 const PORT = process.env.PORT || 5000;
 
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
+
 
 app.listen(PORT, () => 'Server is running on port 5000');
